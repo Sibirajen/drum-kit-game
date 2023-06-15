0f4f87944616ca29be25dec52a5047c0d94bcae9
Sibirajen <119956587+Sibirajen@users.noreply.github.com>
2023-06-15 17:52:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+
+for(var i=0; i<7 ;i++){
+    document.querySelectorAll('button')[i].addEventListener("click",function (){
+        var key = this.innerHTML;
+        sound(key);
+        makeAnimation(key);
+    });
+}
+
+document.addEventListener("keypress",function(event){
+    sound(event.key);
+    makeAnimation(event.key);
+});
+
+function sound(key){
+    switch(key){
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break; 
+        case "k":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;  
+        case "l":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break; 
+    }
+}
+
+function makeAnimation(currKey){
+    var active = document.querySelector("."+currKey);
+    active.classList.add("pressed");
+    setTimeout(function(){
+        active.classList.remove("pressed");
+    },100)
+}
